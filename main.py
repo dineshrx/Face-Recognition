43440c0d03de33e30a4b1645afc7c4bbc54aeb4e
DINESH MATHIVANAN SETHU KUMAR <144202549+dineshrx@users.noreply.github.com>
2024-01-04 09:53:24 +0100

    main.py

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ import face_recognition
 img=cv2.imread("1.jpg") #read the image
 cv2.imshow("test",img) # print the image for testing
 cv2.waitKey(2)
-
+# HOG
 face_location=face_recognition.face_locations(img,model="hog") # detect face in the image
+print("There are {} faces in this image".format(len(face_location)))
+
+#CNN
+face_location=face_recognition.face_locations(img,model="cnn") # detect face in the image
 print("There are {} faces in this image".format(len(face_location)))
 
 # looping through face locations to get its values
